92ada2a33d4c2b7115470bcb0245c4b506ae5df7
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-09-10 19:15:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+PROJETO RECIPE APP - TRYBE
+
+Este projeto foi para fecharmos o módulo de front-end na Trybe, feito inteiramente em React!
+
+Realizado em grupo ( 3 pessoas incluindo eu neste caso ) com a duração de 10 dias.
---
+++ src/components/CategoryFilter/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const CategoryButtons = styled.button`
+  background-color: rgb(232, 93, 4);
+  border-radius: 100px;
+  width: 30%;
+  margin: 0 1%;
+  height: 40px;
+  color: white;
+  font-weight: 700;
+  font-size: 12px;
+  word-wrap: normal;
+  border: ${(props) => (props.Selected ? '1px solid white' : 'none')};
+`;
+
+export const CategorySection = styled.section`
+  width: 90vw;
+  height: 15vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  padding: 10px 0;
+  margin: auto;
+`;
